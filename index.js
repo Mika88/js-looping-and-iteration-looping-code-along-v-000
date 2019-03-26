91b0fc28001f5416bb383f1ea8fa1dd17f16845d
Mika88 <mikaspivak@gmail.com>
2019-03-26 15:42:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function printBadges(names) {
 
 function tailsNeverFails() {
   let count = 0;
-  while ( Math.random() >= 0.5;) {
+  while ( Math.random() >= 0.5) {
     console.log(`You got ${++count} tails in a row!`);
   }
 }
