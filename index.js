8a813efe02f849f5ae18c639787c1bc6dc4ab867
Mika88 <mikaspivak@gmail.com>
2019-03-26 16:05:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function tailsNeverFails() {
   while ( Math.random() >= 0.5) {
     count = ++count
   }
-  console.log(`You got ${++count} tails in a row!`);
+  return console.log(`You got ${++count} tails in a row!`);
 }
